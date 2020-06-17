08a3f7433f7639123b4fab397efa56429489ddff
toutpt <toutpt@gmail.com>
2020-06-17 10:27:49 +0200

    chore: update color
    add missing icon

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,10 @@
 export default (config) => {
-    if (process.NODE_ENV === 'development') {
-        console.log('### dev output');
+    if (process.env.NODE_ENV !== 'development') {
+        console.log('### /fuelcalc dir');
         config.output.publicPath = '/fuelcalc';
+    } else {
+        console.log('### / dir');
+        config.output.publicPath = '/';
     }
     return config;
 };
